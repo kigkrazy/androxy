1258d5320d72e3a52971ea7ddef52776ad0b00d2
kigkrazy <kigkrazy@163.com>
2018-07-18 17:26:30 +0800

    Flowable.interval用例

--- app/src/main/java/com/reizx/asf/view/fragment/SettingFragment.java
+++ app/src/main/java/com/reizx/asf/view/fragment/SettingFragment.java
@@ -1,16 +1,26 @@
 package com.reizx.asf.view.fragment;
 
+import android.annotation.SuppressLint;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.AppUtils;
+import com.blankj.utilcode.util.ShellUtils;
 import com.qmuiteam.qmui.widget.QMUITopBar;
 import com.reizx.asf.R;
 import com.reizx.asf.contract.SettingContract;
 import com.reizx.asf.presenter.SettingPresenter;
 import com.reizx.asf.util.AsfMgrLog;
+import com.reizx.asf.util.RxUtil;
 import com.reizx.asf.view.common.BaseFragment;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class SettingFragment extends BaseFragment<SettingPresenter> implements SettingContract.View {
     @BindView(R.id.topbar)
@@ -19,9 +29,34 @@ public class SettingFragment extends BaseFragment<SettingPresenter> implements S
     @BindView(R.id.tv_setting_page_show_ip_des)
     TextView tvIpStatus;
 
+    Disposable ds;
+
+
+    @SuppressLint("CheckResult")
     @OnClick(R.id.btn_setting_page_test)
     public void clickTest(){
+        if (ds != null && !ds.isDisposed()){
+            AsfMgrLog.d("dispose the subscribe...");
+            ds.dispose();
+            ds = null;
+            return;
+        }
+
         AsfMgrLog.d("click setting page test");
+        ds = Flowable.interval(1,  TimeUnit.SECONDS)
+                .onBackpressureDrop()
+                .compose(RxUtil.<Long>rxSchedulerHelper())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        AsfMgrLog.d("the inter ... " + aLong);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        AsfMgrLog.d("flow err : " + throwable);
+                    }
+                });
     }
 
     @Override
