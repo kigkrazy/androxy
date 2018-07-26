7255c4b1c6ed14e7c74e4f8a9a3defeb45cf6cf4
kigkrazy <kigkrazy@163.com>
2018-07-26 15:16:09 +0800

    测试XLog

---
+++ app/src/main/java/com/reizx/asf/util/XLogUtilExt.java
@@ -0,0 +1,16 @@
+package com.reizx.asf.util;
+
+public class XLogUtilExt {
+    public static class Builder{
+        private Builder(){}
+
+        public Builder newBuilder(){
+            return new Builder();
+        }
+
+
+
+    }
+
+
+}
--- app/src/main/java/com/reizx/asf/view/fragment/SettingFragment.java
+++ app/src/main/java/com/reizx/asf/view/fragment/SettingFragment.java
@@ -3,6 +3,17 @@ package com.reizx.asf.view.fragment;
 import android.annotation.SuppressLint;
 import android.widget.TextView;
 
+import com.elvishew.xlog.LogConfiguration;
+import com.elvishew.xlog.LogLevel;
+import com.elvishew.xlog.XLog;
+import com.elvishew.xlog.flattener.DefaultFlattener;
+import com.elvishew.xlog.interceptor.BlacklistTagsFilterInterceptor;
+import com.elvishew.xlog.printer.AndroidPrinter;
+import com.elvishew.xlog.printer.Printer;
+import com.elvishew.xlog.printer.file.FilePrinter;
+import com.elvishew.xlog.printer.file.backup.FileSizeBackupStrategy;
+import com.elvishew.xlog.printer.file.backup.NeverBackupStrategy;
+import com.elvishew.xlog.printer.file.naming.DateFileNameGenerator;
 import com.qmuiteam.qmui.widget.QMUITopBar;
 import com.reizx.asf.R;
 import com.reizx.asf.contract.SettingContract;
@@ -54,6 +65,11 @@ public class SettingFragment extends BaseFragment<SettingPresenter> implements S
                 });
     }
 
+    @OnClick(R.id.btn_setting_page_xlog)
+    public void printXlog(){
+
+    }
+
     @Override
     public int getFragmentLayoutID() {
         return R.layout.fragment_setting;
@@ -63,6 +79,7 @@ public class SettingFragment extends BaseFragment<SettingPresenter> implements S
     public void initAllMembersView() {
         super.initAllMembersView();
         initTopBar();
+
     }
 
     @Override
@@ -72,6 +89,36 @@ public class SettingFragment extends BaseFragment<SettingPresenter> implements S
 
     public void initTopBar() {
         mTopBar.setTitle("设置");
+    }
+
+    public void initXLog(){
+        LogConfiguration config = new LogConfiguration.Builder()
+                .logLevel(LogLevel.ALL)             // 指定日志级别，低于该级别的日志将不会被打印，默认为 LogLevel.ALL
+                .tag("MY_TAG")                                         // 指定 TAG，默认为 "X-LOG"
+                //.t()                                                   // 允许打印线程信息，默认禁止
+                //.st(2)                                                 // 允许打印深度为2的调用栈信息，默认禁止
+                //.b()                                                   // 允许打印日志边框，默认禁止
+                //.jsonFormatter(new MyJsonFormatter())                  // 指定 JSON 格式化器，默认为 DefaultJsonFormatter
+                //.xmlFormatter(new MyXmlFormatter())                    // 指定 XML 格式化器，默认为 DefaultXmlFormatter
+                //.throwableFormatter(new MyThrowableFormatter())        // 指定可抛出异常格式化器，默认为 DefaultThrowableFormatter
+                //.threadFormatter(new MyThreadFormatter())              // 指定线程信息格式化器，默认为 DefaultThreadFormatter
+                //.stackTraceFormatter(new MyStackTraceFormatter())      // 指定调用栈信息格式化器，默认为 DefaultStackTraceFormatter
+                //.borderFormatter(new MyBoardFormatter())               // 指定边框格式化器，默认为 DefaultBorderFormatter
+                //.addObjectFormatter(AnyClass.class,                    // 为指定类添加格式化器
+                //        new AnyClassObjectFormatter())                     // 默认使用 Object.toString()
+                //.addInterceptor(new BlacklistTagsFilterInterceptor(    // 添加黑名单 TAG 过滤器
+                //        "blacklist1", "blacklist2", "blacklist3"))
+                //.addInterceptor(new MyInterceptor())                   // 添加一个日志拦截器
+                .build();
+        Printer androidPrinter = new AndroidPrinter();             // 通过 android.util.Log 打印日志的打印器
+        Printer filePrinter = new FilePrinter                      // 打印日志到文件的打印器
+                .Builder("/sdcard/xlog/")                              // 指定保存日志文件的路径
+                .fileNameGenerator(new DateFileNameGenerator())        // 指定日志文件名生成器，默认为 ChangelessFileNameGenerator("log")
+                .backupStrategy(new FileSizeBackupStrategy(100 * 1024 * 1024))              // 指定日志文件备份策略，默认为 FileSizeBackupStrategy(1024 * 1024)
+                .logFlattener(new DefaultFlattener())                       // 指定日志平铺器，默认为 DefaultFlattener
+                .build();
+        XLog.init(config, androidPrinter, filePrinter);
+        XLog.d("df");
     }
 
     @Override
