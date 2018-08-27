3cd9920257246396595ee352dcb8826ecea0b3d5
kigkrazy <kigkrazy@163.com>
2018-08-27 15:52:27 +0800

    界面设计完成

--- app/src/main/java/com/reizx/demo/view/fragment/HomeFragment.java
+++ app/src/main/java/com/reizx/demo/view/fragment/HomeFragment.java
@@ -2,18 +2,22 @@ package com.reizx.demo.view.fragment;
 
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.qmuiteam.qmui.widget.QMUITopBar;
 import com.reizx.demo.IAndromedaInf;
 import com.reizx.demo.R;
 import com.reizx.demo.contract.HomeConstract;
 import com.reizx.demo.presenter.HomePresenter;
+import com.reizx.demo.util.AsfLog;
 import com.reizx.demo.view.common.BaseFragment;
 
 import org.qiyi.video.svg.Andromeda;
 
 import butterknife.BindView;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
 public class HomeFragment extends BaseFragment<HomePresenter> implements HomeConstract.View {
@@ -55,6 +59,30 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
         }
     }
 
+    /**
+     * butterknife处理radiobutton
+     * @param view
+     * @param ischanged
+     */
+    @OnCheckedChanged({R.id.rg_home_proxy_mode_ss, R.id.rg_home_proxy_mode_http})
+    public void OnCheckedChangeListener(CompoundButton view, boolean ischanged) {
+        switch (view.getId()) {
+            case R.id.rg_home_proxy_mode_ss:
+                if (ischanged){//注意：这里一定要有这个判断，只有对应该id的按钮被点击了，ischanged状态发生改变，才会执行下面的内容
+                    ToastUtils.showLong("启动ss模式");
+                }
+                break;
+            case R.id.rg_home_proxy_mode_http:
+                if (ischanged) {
+                    //这里写你的按钮变化状态的UI及相关逻辑
+                    ToastUtils.showLong("启动http模式");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     @Override
     public int getFragmentLayoutID() {
         return R.layout.fragment_home;
@@ -73,31 +101,6 @@ public class HomeFragment extends BaseFragment<HomePresenter> implements HomeCon
 
     public void initTopBar() {
         mTopBar.setTitle("主页");
-    }
-
-    @Override
-    public void showErrorMsg(String msg) {
-
-    }
-
-    @Override
-    public void stateError() {
-
-    }
-
-    @Override
-    public void stateEmpty() {
-
-    }
-
-    @Override
-    public void stateLoading() {
-
-    }
-
-    @Override
-    public void stateMain() {
-
     }
 
     @Override
